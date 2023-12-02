1dfa605321639956459551056933afb10d799c39
Nate Reprogle <nate.reprogle@gmail.com>
2023-12-02 09:32:29 -0600

    Fix cariage return on Windows
    When I did part 2 of day 2, I manually switched the linefeed characters in VSCode to LF from CRLF. However, this will fix it automatically from now on

--- 2023/utils/reader.ts
+++ 2023/utils/reader.ts
@@ -1,14 +1,16 @@
-import { readFileSync } from "fs";
-import { join } from "path";
+import { readFileSync } from 'fs'
+import { join } from 'path'
 
 export function getInput(dayNumber: number) {
-    const dayDirectory = `day-${dayNumber}`;
-    const filePath = join(__dirname, '..', dayDirectory, 'input.txt');
+    const dayDirectory = `day-${dayNumber}`
+    const filePath = join(__dirname, '..', dayDirectory, 'input.txt')
 
     try {
-        return readFileSync(filePath, 'utf-8');
+        return readFileSync(filePath, 'utf-8').replace(/\r/g, '').trim()
     } catch (error: any) {
-        console.error(`Error reading input file for day ${dayNumber}: ${error.message}`);
-        return null;
+        console.error(
+            `Error reading input file for day ${dayNumber}: ${error.message}`
+        )
+        return null
     }
 }
